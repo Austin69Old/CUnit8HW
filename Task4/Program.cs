ec746fd4f35a860aaf366fd5cd6d1e9e54b32dde
Maksim <craash00077@gmail.com>
2023-10-29 16:40:06 +0300

    FinalTask

---
+++ Task4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FinalTask
+{
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+        public Student(string name, string group, DateTime dateofbirth)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = dateofbirth;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\craas\OneDrive\Рабочий стол\TestFolder\Students.dat";
+           
+            ReadFile(path);
+        }
+
+        static void ReadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+
+                BinaryFormatter bf = new BinaryFormatter();
+                using (var fs = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    var Students = (Student[])bf.Deserialize(fs);
+                    foreach (var Student in Students)
+                    {
+                        Console.WriteLine($"Имя: {Student.Name}");
+                        Console.WriteLine($"Группа: {Student.Group}");
+                        Console.WriteLine($"Дата рождения: {Student.DateOfBirth}");
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Файла не существует");
+            }
+        }
+    }
+}
